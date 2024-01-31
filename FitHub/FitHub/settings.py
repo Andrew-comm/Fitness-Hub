5093c41ea969dc93f3a68b80b07e1d6fe61b9e31
andrea <ndrwrono2001@gmail.com>
2024-01-31 23:56:57 +0300

    added environment variables

--- FitHub/FitHub/settings.py
+++ FitHub/FitHub/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,10 +101,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('CUSTOM_NAME'),
+        'USER': os.getenv('CUSTOM_USER'),
+        'PASSWORD': os.getenv('CUSTOM_PASSWORD'),
+        'HOST': os.getenv('CUSTOM_HOST'),
+        'PORT': os.getenv('CUSTOM_PORT'),
     }
 }
 
